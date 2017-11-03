70b43782888ded953867af32f52e55143240011a
student <student@GFL-1615.ad.geeksforless.net>
2017-11-03 18:43:36 +0200

    bootstrap theme

--- views/includes/headers.php
+++ views/includes/headers.php
@@ -4,6 +4,11 @@
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <title></title>
     <link rel="stylesheet" href="/css/main.css" type="text/css" />
+    <!-- Bootstrap -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/bootstrap.min" rel="stylesheet">
+    <link href="/css/bootstrap-theme.min" rel="stylesheet">
+    <link href="/css/bootstrap-theme.min.css.map" rel="stylesheet">
 </head>
 <body>
 <div id="wrapperDiv">
